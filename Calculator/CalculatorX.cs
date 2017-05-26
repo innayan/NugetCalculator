01c884f883832973b7f6c90b2765d9291c0a151e
innayan <inna_yan@mail.ru>
2017-05-26 16:57:14 +0300

    qq

---
+++ Calculator/CalculatorX.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class CalculatorX
+    {
+       public static int Summa(int X, int Y)
+            {
+                return X + Y;
+            }
+
+            public static int Difference(int X, int Y)
+            {
+                return X - Y;
+            }
+
+            public static int Composition(int X, int Y)
+            {
+                return X * Y;
+            }
+
+        }
+ }
